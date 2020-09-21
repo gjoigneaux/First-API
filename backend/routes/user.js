758a0286f72a6315c1b30957d2bd21847cc91a9a
gjoigneaux <60109809+gjoigneaux@users.noreply.github.com>
2020-09-21 14:21:13 +0200

    Ajouter Bouncer

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
-
+const bouncer = require('express-bouncer')(1000, 2000, 3);//(Mini attend, max attente, nombre de tentative)
 const userCtrl = require('../controllers/user');
 
+//permet de bloquer les tentative de connexion par force brute
+bouncer.blocked = function (req, res, next, remaining)
+{
+	res.send (429, "Too many requests have been made, " +
+    "please wait " + remaining / 100 + " seconds");
+};
+
 router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
+router.post('/login', bouncer.block, userCtrl.login);
 
 module.exports = router;
